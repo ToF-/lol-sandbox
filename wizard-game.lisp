8360e318f63fa5b7d2877627f2e5198387720870
Christophe Thibaut <cthibauttof@gmail.com>
2023-09-17 21:54:43 +0200

    refactoring

--- wizard-game.lisp
+++ wizard-game.lisp
@@ -1,11 +1,11 @@
-(defparameter *nodes* '((living-room (you are in the living room. a wizard is snoring loudly on the couch.))
+(defparameter *wizard-nodes* '((living-room (you are in the living room. a wizard is snoring loudly on the couch.))
                        (garden (you are in a beautiful garden. there is a well in front of you.))
                        (attic (you are in the attic. there is a giant welding torch in the corner.))))
 
 (defun describe-location (location nodes)
   (cadr (assoc location nodes)))
 
-(defparameter *edges* '((living-room (garden west door)
+(defparameter *wizard-edges* '((living-room (garden west door)
                                      (attic upstairs ladder))
                         (garden (living-room east door))
                         (attic (living-room downstairs ladder))))
@@ -36,13 +36,13 @@
 (defparameter *location* 'living-room)
 
 (defun look ()
-  (append (describe-location *location* *nodes*)
-          (describe-paths *location* *edges*)
+  (append (describe-location *location* *wizard-nodes*)
+          (describe-paths *location* *wizard-edges*)
           (describe-objects *location* *objects* *object-locations*)))
 
 (defun walk (direction)
   (let ((next (find direction
-                    (cdr (assoc *location* *edges*))
+                    (cdr (assoc *location* *wizard-edges*))
                     :key #'cadr)))
     (if next
       (progn (setf *location* (car next))
